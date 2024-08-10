cc0855b9dd159cbbc39aa5f6c0dacbed57d82438
Kanahia <kaushalkanahia@gmail.com>
2024-08-10 14:43:48 +0530

    Initial - 2

--- app/src/androidTest/java/com/kanahia/unit/MainActivityTest.kt
+++ app/src/androidTest/java/com/kanahia/unit/MainActivityTest.kt
@@ -3,7 +3,10 @@ package com.kanahia.unit
 import android.annotation.SuppressLint
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.typeText
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.activityScenarioRule
 import org.junit.Assert.*
 import org.junit.Rule
@@ -17,6 +20,8 @@ class MainActivityTest{
     @SuppressLint("CheckResult")
     @Test
     fun testEditView_OnButtonClick(){
+        Espresso.onView(withId(R.id.editText)).perform(typeText("DEMO"))
         Espresso.onView(withId(R.id.button)).perform(click())
+        Espresso.onView(withId(R.id.textView)).check(matches(withText("DEMO")))
     }
 }
